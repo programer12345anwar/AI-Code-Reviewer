bf51f5f819a16836cead010064fcef515106dafa
Md Anwar Alam <mdanwar40212@gmail.com>
2025-12-25 21:42:36 +0530

    added backend and configured with database

---
+++ backend/aicodereview/src/main/java/com/anwar/aicodereview/AicodereviewApplication.java
@@ -0,0 +1,13 @@
+package com.anwar.aicodereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicodereviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicodereviewApplication.class, args);
+	}
+
+}
